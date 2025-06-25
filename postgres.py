12cba5b11475e7d81deec3aad7e29057bcc0af6e
Michael Kalantar <kalantar@us.ibm.com>
2025-06-25 06:16:50 -0400

    change logging statement

--- postgres.py
+++ postgres.py
@@ -24,6 +24,7 @@ def execute(connection, sql : str) -> None:
         cursor.execute(sql)
         connection.commit()
     except Exception as e:
-        logging.exception("error executing sql statement", e)
+        logging.error(f"error executing sql statement {e}")
+        # logging.exception("error executing sql statement", e)
     finally:
         cursor.close()
